ba1aef5785717881d99b08c12cc4931782c5c659
dongduy956 <dongduy0612@gmail.com>
2021-01-08 21:03:20 +0700

    RoundRobin

---
+++ RoundRobin/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace RoundRobin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+        RoundRobin a = new RoundRobin();
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            bool check = true;
+            try
+            {
+                int tgden = int.Parse(txtgden.Text);
+            }
+            catch
+            {
+                check = false;
+                MessageBox.Show("Lỗi nhập thời gian đến", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            try
+            {
+                int tgxuly = int.Parse(txBtgxuly.Text);
+            }
+            catch
+            {
+                check = false;
+                MessageBox.Show("Lỗi thời gian xử lý", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if (check == true)
+            {
+                ListViewItem item1 = new ListViewItem();
+                item1.Text = txBTen.Text;
+                item1.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = txtgden.Text });
+                item1.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = txBtgxuly.Text });
+                lwinput.Items.Add(item1);
+
+                a.add(txBTen.Text, int.Parse(txtgden.Text), int.Parse(txBtgxuly.Text));
+                txBquarium.Enabled = true;
+                txBTen.Text = txtgden.Text = txBtgxuly.Text = "";
+            }
+        }
+
+        private void txBTen_TextChanged(object sender, EventArgs e)
+        {
+            if (txBTen.Text == "")
+                btnadd.Enabled = false;
+            if (txtgden.Text != "" && txBTen.Text != "" && txBtgxuly.Text != "")
+                btnadd.Enabled = true;
+        }
+
+        private void txtgden_TextChanged(object sender, EventArgs e)
+        {
+            if (txtgden.Text == "")
+                btnadd.Enabled = false;
+            if (txtgden.Text != "" && txBTen.Text != "" && txBtgxuly.Text != "")
+                btnadd.Enabled = true;
+        }
+
+        private void txBtgxuly_TextChanged(object sender, EventArgs e)
+        {
+            if (txBtgxuly.Text == "")
+                btnadd.Enabled = false;
+            if (txtgden.Text != "" && txBTen.Text != "" && txBtgxuly.Text != "")
+                btnadd.Enabled = true;
+        }
+        List<tientrinh> gant = new List<tientrinh>();
+        List<tientrinh> tghtcho = new List<tientrinh>();
+
+        private void btnaddtg_Click(object sender, EventArgs e)
+        {
+            bool check = true;
+            try
+            {
+                int tgden = int.Parse(txBquarium.Text);
+            }
+            catch
+            {
+                check = false;
+                MessageBox.Show("Lỗi nhập thời gian đến", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if (check == true)
+            {
+                a.quanTum = int.Parse(txBquarium.Text);
+                btnGant.Enabled = BTSUMIT.Enabled = true;
+            }
+        }
+
+        private void txBquarium_TextChanged(object sender, EventArgs e)
+        {
+            if (txBquarium.Text != "")
+                btnaddtg.Enabled = true;
+            else
+                btnaddtg.Enabled = false;
+        }
+
+        private void BTSUMIT_Click(object sender, EventArgs e)
+        {
+            if (a.quanTum == -1 || a.xuLy(a.quanTum) == null)
+                MessageBox.Show("Lỗi dữ liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                tghtcho = a.thoigianhtcho(a.quanTum);
+                gant = a.xuLy(a.quanTum);
+                listView1.View = View.Details;
+                for (int i = 0; i < tghtcho.Count; i++)
+                {
+                    ListViewItem item1 = new ListViewItem();
+                    item1.Text = tghtcho[i].TenTienTrinh;
+                    item1.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = tghtcho[i].ThoiGianCho.ToString() });
+                    item1.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = tghtcho[i].ThoiGianXuLy.ToString() });
+                    listView1.Items.Add(item1);
+                }
+                tghttb.Text = a.tinhThoiGianHTTB(int.Parse(txBquarium.Text)).ToString();
+                lbtgchotb.Text = a.tinhThoiGianChoTB(int.Parse(txBquarium.Text)).ToString();
+            }
+        }
+
+        private void btnGant_Click(object sender, EventArgs e)
+        {
+            if (a.quanTum == -1 || a.xuLy(a.quanTum) == null)
+                MessageBox.Show("Lỗi dữ liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                  
+                gant = a.xuLy(a.quanTum);
+                groupBox4.Text = "GANT";
+                groupBox4.Visible = true;
+                int x = 16;
+                Label lb1 = new Label();
+                lb1.Text = gant[0].ThoiGianCho.ToString();
+                lb1.Size = new System.Drawing.Size(36, 20);
+                lb1.Location = new Point(10, 48);
+                groupBox4.Controls.Add(lb1);
+                Random rnd = new Random();
+                for (int i = 0; i < gant.Count; i++)
+                {                  
+                    Button btn = new Button();
+                    btn.AutoSize = true;
+                    Color rand = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));                 
+                    btn.BackColor = rand ;                    
+                    btn.Text = gant[i].TenTienTrinh;
+                    btn.Size = new System.Drawing.Size(36, 20);
+                    btn.Location = new Point(x, 22);
+                    x += btn.Size.Width - 1;
+                    Label lb = new Label();
+                    lb.Text = gant[i].ThoiGianXuLy.ToString();
+                    lb.Size = new System.Drawing.Size(36, 20);
+                    lb.Location = new Point(btn.Location.X + 27, 48);
+                    groupBox4.Controls.Add(btn);
+                    groupBox4.Controls.Add(lb);
+                }
+            }
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {         
+            foreach (ListViewItem item in lwinput.SelectedItems)
+            {
+                string name = item.Text;
+                lwinput.Items.Remove(item);            
+                for (int i = 0; i < a.Tam.Count; i++)
+                    if (name.Equals(a.Tam[i].TenTienTrinh) == true)
+                    { a.Tam.RemoveAt(i); break; }
+            }           
+               listView1.Items.Clear();
+            groupBox4.Controls.Clear();
+            lbtgchotb.Text = tghttb.Text = "";
+            groupBox4.Text = "";
+            groupBox4.Visible = false;
+        }
+
+    }
+}
---
+++ RoundRobin/RoundRoubin.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundRobin
+{
+    public class RoundRobin
+    {
+        Queue<tientrinh> tientrinhf = new Queue<tientrinh>();
+        List<tientrinh> tam = new List<tientrinh>();
+        public int quanTum = -1;
+        public List<tientrinh> Tam
+        {
+            get { return tam; }
+            set { tam = value; }
+        }
+        public Queue<tientrinh> Tientrinhf
+        {
+            get { return tientrinhf; }
+            set { tientrinhf = value; }
+        }
+        public void add(string ten1, int tgden1, int tgxuly1)
+        {
+
+            tientrinh a = new tientrinh(ten1, tgden1, tgxuly1);
+            tam.Add(a);
+        }
+        void them()
+        {
+            tam = tam.OrderBy(t => t.ThoiGianDen).ToList();
+            for (int i = 0; i < tam.Count; i++)
+                tientrinhf.Enqueue(tam[i]);
+        }
+        public List<tientrinh> xuLy(int timeSlice)
+        {
+            tientrinhf.Clear();
+            them();
+            if (tientrinhf.Count == 0)
+                return null;
+            Queue<tientrinh> tam = new Queue<tientrinh>();
+            List<tientrinh> gant = new List<tientrinh>();
+            int c = 0;
+
+            tientrinh a = tientrinhf.Peek();
+            if (timeSlice >= a.ThoiGianXuLy)
+                gant.Add(new tientrinh(a.TenTienTrinh, a.ThoiGianDen, a.ThoiGianDen, a.ThoiGianDen + a.ThoiGianXuLy));
+            else
+            {
+                gant.Add(new tientrinh(a.TenTienTrinh, a.ThoiGianDen, a.ThoiGianDen, timeSlice + a.ThoiGianDen));
+                c = 1;
+            }
+            tientrinh x;
+            x = tientrinhf.Dequeue();
+            while (tientrinhf.Count != 0 && tientrinhf.Peek().ThoiGianDen < gant[gant.Count - 1].ThoiGianXuLy)
+            {
+                tientrinh y;
+                y = tientrinhf.Dequeue();
+                tam.Enqueue(y);
+            }
+            if (c == 1)
+                tam.Enqueue(new tientrinh(x.TenTienTrinh, timeSlice, x.ThoiGianDen, x.ThoiGianXuLy - timeSlice));
+            while (tam.Count != 0)
+            {
+                c = 0;
+                if (timeSlice >= tam.Peek().ThoiGianXuLy)
+                    gant.Add(new tientrinh(tam.Peek().TenTienTrinh, gant[gant.Count - 1].ThoiGianXuLy, tam.Peek().ThoiGianDen, tam.Peek().ThoiGianXuLy + gant[gant.Count - 1].ThoiGianXuLy));
+                else
+                {
+                    gant.Add(new tientrinh(tam.Peek().TenTienTrinh, gant[gant.Count - 1].ThoiGianXuLy, tam.Peek().ThoiGianDen, timeSlice + gant[gant.Count - 1].ThoiGianXuLy));
+                    c = 1;
+                }
+                while (tientrinhf.Count != 0 && tientrinhf.Peek().ThoiGianDen < gant[gant.Count - 1].ThoiGianXuLy)
+                {
+                    tientrinh y = tientrinhf.Dequeue();
+                    tam.Enqueue(y);
+                }
+                tientrinh t = tam.Dequeue();
+                if (c == 1)
+                    tam.Enqueue(new tientrinh(t.TenTienTrinh, timeSlice, t.ThoiGianDen, t.ThoiGianXuLy - timeSlice));
+
+            }
+            return gant;
+        }
+        private tientrinh tinhThoiGianHT(int timeSlice, tientrinh x)
+        {
+            tientrinh max = new tientrinh();           
+            List<tientrinh> gant = xuLy(timeSlice);
+            for (int i = 0; i < gant.Count; i++)
+                if (gant[i].TenTienTrinh.Equals(x.TenTienTrinh) == true)
+                    max = gant[i];
+            max.ThoiGianXuLy -= max.ThoiGianDen;
+            return max;
+        }
+        public float tinhThoiGianHTTB(int timeSlice)
+        {
+            int dem = 0;
+            int tong = 0;
+            for (int i = 0; i < tam.Count; i++)
+            {
+                dem++;
+                tong += tinhThoiGianHT(timeSlice, tam[i]).ThoiGianXuLy;
+            }
+            return tong / (float)dem;
+        }
+        List<tientrinh> thoigian = new List<tientrinh>();
+        public List<tientrinh> tinhThoiGianHTTB1(int timeSlice)
+        {
+            thoigian.Clear();
+            for (int i = 0; i < tam.Count; i++)
+            {
+                thoigian.Add(tinhThoiGianHT(timeSlice, tam[i]));
+            }
+            return thoigian;
+        }
+        private int tinhThoiGianCho(int timeSlice, tientrinh x)
+        {
+            tientrinh q = new tientrinh();
+            List<tientrinh> gant = xuLy(timeSlice);
+            int vt = -1;
+            for (int i = 0; i < gant.Count; i++)
+                if (x.TenTienTrinh.Equals(gant[i].TenTienTrinh) == true)
+                {
+                    vt = i;
+                    q = gant[i]; break;
+                }
+            int tongCho = q.ThoiGianCho - q.ThoiGianDen;
+            for (int i = vt; i < gant.Count; i++)
+                if (x.TenTienTrinh.Equals(gant[i].TenTienTrinh) == true)
+                    for (int t = i + 1; t < gant.Count; t++)
+                        if (x.TenTienTrinh.Equals(gant[t].TenTienTrinh) == true)
+                        { tongCho += gant[t].ThoiGianCho - gant[i].ThoiGianXuLy; break; }
+            return tongCho;
+        }
+        public float tinhThoiGianChoTB(int timeSlice)
+        {
+            int dem = 0;
+            int tong = 0;
+            List<tientrinh> gant = xuLy(timeSlice);
+            for (int p = 0; p < tam.Count; p++)
+            {
+                dem++;
+
+                tong += tinhThoiGianCho(timeSlice, tam[p]);
+            }
+            return tong / (float)dem;
+        }
+        public List<tientrinh> thoigianhtcho(int timeSlice)
+        {
+            thoigian = tinhThoiGianHTTB1(timeSlice);
+            List<tientrinh> gant = xuLy(timeSlice);
+            for (int p = 0; p < tam.Count; p++)
+            {
+                for (int g = 0; g < thoigian.Count; g++)
+                    if (thoigian[g].TenTienTrinh.Equals(tam[p].TenTienTrinh) == true)
+                    { thoigian[g].ThoiGianCho = tinhThoiGianCho(timeSlice, tam[p]); break; }
+            }
+            return thoigian;
+        }
+    }
+}
